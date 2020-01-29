ce79f53ca3fee6ead2060607ab25c41169504f18
Mi-lex <mishin-lex@mail.ru>
2020-01-29 08:44:39 +0300

    add func for gettins formatted str date

--- resources/js/utils.ts
+++ resources/js/utils.ts
@@ -5,3 +5,16 @@
 export const classesExtractor = (classes, classNames: string[]): string => {
     return classNames.map(name => classes[name]).join(' ');
 };
+
+export const getDateString = (date: Date): string => {
+    const day: string = date
+        .getDate()
+        .toString()
+        .padStart(2, '0');
+
+    const month: string = (date.getMonth() + 1).toString().padStart(2, '0');
+
+    const year: number = date.getFullYear();
+
+    return `${day}.${month}.${year}`;
+};
