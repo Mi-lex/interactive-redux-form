58965648738e2c8a1d838388821926cb5033657d
Mi-lex <mishin-lex@mail.ru>
2020-01-31 13:59:09 +0300

    insert actual icons

--- resources/js/assets/icons.ts
+++ resources/js/assets/icons.ts
@@ -1,5 +1,19 @@
-import css3 from '../../img/icons/css3.svg';
+import createNewPassport from '../../img/icons/create_new.svg';
+import search from '../../img/icons/search.svg';
+import addPassport from '../../img/icons/add_passport.svg';
+import approveForm from '../../img/icons/approve.svg';
+import print from '../../img/icons/print.svg';
+import calendar from '../../img/icons/calendar.svg';
+import newRow from '../../img/icons/new_row.svg';
+import deleteRow from '../../img/icons/delete_row.svg';
 
 export default {
-    css3,
+    createNewPassport,
+    search,
+    addPassport,
+    approveForm,
+    print,
+    calendar,
+    newRow,
+    deleteRow,
 };
