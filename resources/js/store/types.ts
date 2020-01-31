e2498a2695b44afaa4b4544c6ab2cb653666da14
Mi-lex <mishin-lex@mail.ru>
2020-01-31 13:57:46 +0300

    add some reusable types

---
+++ resources/js/store/types.ts
@@ -0,0 +1,16 @@
+import icons from '../assets/icons';
+
+export type IconName = keyof typeof icons;
+export type ElementSize = 'sm' | 'md' | 'big';
+
+export enum FixingPapersType {
+    paperClip = 'Скрепка',
+    termo = 'Термо',
+    spring = 'Пружина',
+    packet = 'Пакет',
+    splice = 'Склейка',
+    binding = 'Переплет',
+    file = 'Папка',
+};
+
+export type FixingPapersTypeString = keyof typeof FixingPapersType;
