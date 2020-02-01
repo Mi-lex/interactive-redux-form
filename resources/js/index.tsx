8352c23ccf49ebc4b8897070c40360c8f893f6b4
Mi-lex <mishin-lex@mail.ru>
2020-02-01 15:17:41 +0300

    separate store from reducer

--- resources/js/index.tsx
+++ resources/js/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
-import { rootStore } from './store/rootReducer';
+import { rootStore } from './store';
 import '../css/style.css';
 
 const ReduxApp: React.FC = () => {
