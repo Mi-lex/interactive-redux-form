c415fce6f887ca405ee7759b185d165a5208db4f
Mi-lex <mishin-lex@mail.ru>
2020-02-01 15:14:01 +0300

    put options data in separate file

---
+++ resources/js/store/data.ts
@@ -0,0 +1,99 @@
+import { InputOptions } from './types';
+
+export const inputOptions: InputOptions = {};
+
+inputOptions.managers = [
+    {
+        value: 'oleg',
+        label: 'Олег',
+    },
+    {
+        value: 'vladimir',
+        label: 'Владимир',
+    },
+];
+
+inputOptions.organizations = [
+    {
+        value: 'apple',
+        label: 'Apple',
+    },
+    {
+        value: 'raduga',
+        label: 'Радуга',
+    },
+];
+
+inputOptions.termoPositions = [
+    {
+        value: 'equal',
+        label: 'Вровень с блоком',
+    },
+    {
+        value: 'right',
+        label: 'правее блока',
+    },
+];
+
+inputOptions.packetTypes = [
+    {
+        value: 'circles',
+        label: 'люверсы',
+    },
+    {
+        value: 'tshirt',
+        label: 'майка',
+    },
+];
+
+inputOptions.packetHandles = [
+    {
+        value: 'cut',
+        label: 'ручка-вырез',
+    },
+    {
+        value: 'fabric',
+        label: 'тканевые',
+    },
+];
+
+inputOptions.spliceMaterial = [
+    {
+        value: 'glue',
+        label: 'клей',
+    },
+    {
+        value: 'resin',
+        label: 'смола?',
+    },
+];
+
+inputOptions.splicePositions = [
+    {
+        value: 'сверху',
+        label: 'up',
+    },
+    {
+        value: 'снизу',
+        label: 'down',
+    },
+    {
+        value: 'слева',
+        label: 'left',
+    },
+    {
+        value: 'справа',
+        label: 'right',
+    },
+];
+
+inputOptions.orderWrapMaterials = [
+    {
+        value: 'matte',
+        label: 'матовый',
+    },
+    {
+        value: 'glossy',
+        label: 'глянцевый',
+    },
+];
