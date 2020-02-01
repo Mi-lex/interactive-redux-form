62de31609f90b10516987f034956cb77f72b8b96
Mi-lex <mishin-lex@mail.ru>
2020-02-01 15:18:41 +0300

    replace autocomplete component with redux creative select in order elements table

--- resources/js/components/OrderElementsTable.tsx
+++ resources/js/components/OrderElementsTable.tsx
@@ -1,7 +1,8 @@
-import React, { ReactNode } from 'react';
-import { Field } from 'redux-form';
-import AutocompleteInput from './AutocompleteInput';
+import React, { ReactNode, FC } from 'react';
+import { Field, InjectedArrayProps } from 'redux-form';
 import PassportBtn from './PassportBtn';
+import ReduxCreatableSelect from './ReduxCreatableSelect';
+import { inputOptions } from '../store/data';
 
 type OrderElementsFieldPropType = {
     name: string;
@@ -12,9 +13,14 @@ const OrderElementsField: React.FC = ({ name = 'orderElement' }: OrderElementsFi
     <div className="row orderElement">
         <Field name={`${name}.name`} component="input" type="text" />
         <Field name={`${name}.paperCount`} component="input" type="number" />
-        <Field name={`${name}.wrapMaterial`} component={AutocompleteInput}>
-            {['матовый', 'глянцевый']}
-        </Field>
+        <Field
+            name={`${name}.wrapMaterial`}
+            className="!later"
+            placeholder=""
+            component={ReduxCreatableSelect}
+            isClearable={true}
+            options={inputOptions.orderWrapMaterials}
+        />
         <Field name={`${name}.printType`} component="input" type="text" />
         <Field name={`${name}.colorFormula`} component="input" type="text" />
         <Field name={`${name}.colorInterpretation`} component="input" type="text" />
@@ -22,15 +28,31 @@ const OrderElementsField: React.FC = ({ name = 'orderElement' }: OrderElementsFi
     </div>
 );
 
-const OrderElementsTable = ({ fields }) => (
+type OrderElementsTableProps = {
+    fields: InjectedArrayProps & Array<string>;
+};
+
+const OrderElementsTable = ({ fields }: OrderElementsTableProps) => (
     <>
         {fields.map((order, index) => (
             <OrderElementsField key={index} name={`${order}Element`} />
         ))}
 
         <div className="row orderElementsController">
-            <PassportBtn size="sm" iconName={'newRow'} onClick={() => fields.push()} />
-            <PassportBtn size="sm" iconName={'deleteRow'} onClick={() => fields.pop()} />
+            <PassportBtn
+                size="sm"
+                iconName={'newRow'}
+                onClick={(): void => {
+                    fields.push();
+                }}
+            />
+            <PassportBtn
+                size="sm"
+                iconName={'deleteRow'}
+                onClick={(): void => {
+                    fields.pop();
+                }}
+            />
         </div>
     </>
 );
