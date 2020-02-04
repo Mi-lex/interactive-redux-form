5a7e40cfd6c0804ac41ea5235d015e95bb08bfc1
Mi-lex <mishin-lex@mail.ru>
2020-02-04 23:40:46 +0300

    build layout for order elements table

--- resources/js/components/OrderElementsTable.tsx
+++ resources/js/components/OrderElementsTable.tsx
@@ -3,6 +3,7 @@ import { Field, WrappedFieldArrayProps } from 'redux-form';
 import PassportBtn from './PassportBtn';
 import ReduxCreatableSelect from './ReduxCreatableSelect';
 import { inputOptions } from '../store/data';
+import classes from '../../css/modules/OrderElementsTable.module.css';
 
 type OrderElementsFieldPropType = {
     name: string;
@@ -10,28 +11,51 @@ type OrderElementsFieldPropType = {
 };
 
 const OrderElementsField = ({ name = 'orderElement' }: OrderElementsFieldPropType): JSX.Element => (
-    <div className="row orderElement">
-        <Field name={`${name}.name`} component="input" type="text" />
-        <Field name={`${name}.paperCount`} component="input" type="number" />
+    <div className="d-flex justify-content-around">
         <Field
+            className={`passInputBorder passInputSize passInputmb ${classes.name}`}
+            name={`${name}.name`}
+            component="input"
+            type="text"
+        />
+        <Field
+            className={`passInputBorder passInputSize passInputmb  ${classes.pages}`}
+            name={`${name}.paperCount`}
+            component="input"
+            type="number"
+        />
+        <Field
+            className={`passInputmb  ${classes.wrapMaterial}`}
             name={`${name}.wrapMaterial`}
-            className="!later"
             placeholder=""
             component={ReduxCreatableSelect}
-            isClearable={true}
             options={inputOptions.orderWrapMaterials}
         />
         <Field
+            className={`passInputSize passInputmb  ${classes.printType}`}
             name={`${name}.printType`}
-            className="!later"
             placeholder="уф-принтер"
             component={ReduxCreatableSelect}
-            isClearable={true}
             options={inputOptions.printTypes}
         />
-        <Field name={`${name}.colorFormula`} component="input" type="text" />
-        <Field name={`${name}.colorInterpretation`} component="input" type="text" />
-        <Field name={`${name}.additionalInfo`} component="input" type="text" />
+        <Field
+            className={`passInputBorder passInputSize passInputmb  ${classes.colorFormula}`}
+            name={`${name}.colorFormula`}
+            component="input"
+            type="text"
+        />
+        <Field
+            className={`passInputBorder passInputSize passInputmb  ${classes.colorInterpretation}`}
+            name={`${name}.colorInterpretation`}
+            component="input"
+            type="text"
+        />
+        <Field
+            className={`passInputBorder passInputSize passInputmb  ${classes.addInfo}`}
+            name={`${name}.additionalInfo`}
+            component="input"
+            type="text"
+        />
     </div>
 );
 
@@ -41,7 +65,7 @@ const OrderElementsTable = ({ fields }: WrappedFieldArrayProps<string>): JSX.Ele
             <OrderElementsField key={index} name={`${order}Element`} />
         ))}
 
-        <div className="row orderElementsController">
+        <div className="d-flex justify-content-end orderElementsController">
             <PassportBtn
                 size="sm"
                 iconName={'newRow'}
