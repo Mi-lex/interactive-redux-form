e6c1c56b764c72dab8501b7c2a101a683277fddd
Mi-lex <mishin-lex@mail.ru>
2020-02-04 23:37:39 +0300

    customize creatable select

--- resources/js/components/ReduxCreatableSelect.tsx
+++ resources/js/components/ReduxCreatableSelect.tsx
@@ -3,17 +3,43 @@ import { WrappedFieldProps } from 'redux-form';
 import CreatableSelect from 'react-select/creatable';
 import { InputOptions } from '../store/types';
 
-type Props = InputOptions & WrappedFieldProps;
+type Props = InputOptions &
+    WrappedFieldProps & {
+        className?: string;
+    };
+
+const customStyles = {
+    option: provided => ({
+        ...provided,
+        borderColor: '#FFE082',
+    }),
+    control: provided => ({
+        ...provided,
+        borderRadius: '8px',
+        height: '100%',
+        minHeight: '30px',
+        borderColor: '#FFE082',
+    }),
+    indicatorSeparator: () => ({
+        display: 'none',
+    }),
+    dropdownIndicator: provided => ({
+        ...provided,
+        color: '#FFE082',
+    }),
+};
 
 const ReduxCreatableSelect = (props: Props): JSX.Element => {
-    const { input, options, ...restProps } = props;
+    const { input, className = '', options, ...restProps } = props;
     return (
         <CreatableSelect
             {...input}
             {...restProps}
+            className={className}
             onChange={(value): void => {
                 input.onChange(value);
             }}
+            styles={customStyles}
             onBlur={(): void => {
                 input.onBlur(input.value);
             }}
