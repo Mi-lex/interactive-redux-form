0f21b38b715ca92d5d566a6c1d20f4b99a11d5d1
Mi-lex <mishin-lex@mail.ru>
2020-02-05 00:10:28 +0300

    add public folder for heroku

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,10 @@
     <meta name"keywords" content="">
     <meta name="author" content="">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link href="/app.css" rel="stylesheet"></head>
+    <link href="./app.css" rel="stylesheet"></head>
 </head>
 <body>
    <div id="root"></div> 
-    <script type="text/javascript" src="/app.js"></script></body>
+    <script type="text/javascript" src="./app.js"></script></body>
 </body>
 </html>
