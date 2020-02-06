7ed63ee2bccc978228805f9405b0551044fd3119
Mi-lex <mishin-lex@mail.ru>
2020-02-06 23:21:00 +0300

    configure spacing for form

--- resources/js/pages/PassportForm.tsx
+++ resources/js/pages/PassportForm.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
 import { reduxForm } from 'redux-form';
 import { inputOptions } from '../store/data';
 import FormControl from '../components/FormControl';
 import InitialForm from '../components/InitialForm';
 import OrderElementsForm from '../components/OrderElementsTable';
 import FinalForm from '../components/FinalForm';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    spacing: 6,
+});
+
+theme.spacing(2);
 
 const PassportForm = (): JSX.Element => {
     return (
-        <div className="container-fluid">
-            <form action="POST" className="passportForm">
-                <FormControl />
-                <InitialForm />
-                <OrderElementsForm />
-                <FinalForm />
-            </form>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Container>
+                <form action="POST" className="passportForm">
+                    <FormControl />
+                    <InitialForm />
+                    <OrderElementsForm />
+                    <FinalForm />
+                </form>
+            </Container>
+        </ThemeProvider>
     );
 };
 
