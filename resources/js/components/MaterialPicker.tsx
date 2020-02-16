2f2c0d00cfa22092aaef83544b6cc5be72ab7d3d
Mi-lex <mishin-lex@mail.ru>
2020-02-16 19:20:02 +0300

    make material datepicker component compitable with field

---
+++ resources/js/components/MaterialPicker.tsx
@@ -0,0 +1,54 @@
+/**
+ * Component represents combine version of Redux Form Field
+ * and material date picker
+ */
+import React, { useState, useEffect } from 'react';
+import { KeyboardDatePicker, KeyboardDatePickerProps } from '@material-ui/pickers';
+import { WrappedFieldProps, BaseFieldProps, Field } from 'redux-form';
+
+interface WrappedPickerProps extends WrappedFieldProps, KeyboardDatePickerProps {}
+
+const RenderDateInput = (props: WrappedPickerProps): JSX.Element => {
+    const {
+        input,
+        label,
+        name,
+        meta: { touched, error },
+        ...rest
+    } = props;
+    const [selectedDate, setValue] = useState(() => {
+        return input.value ? new Date(input.value) : null;
+    });
+
+    useEffect(() => {
+        if (selectedDate) {
+            const date = new Date(selectedDate).getTime();
+            input.onChange(date);
+        }
+
+        if (!selectedDate && input.value) {
+            setValue(new Date(input.value));
+        }
+    });
+
+    return (
+        <>
+            <KeyboardDatePicker
+                autoOk
+                {...input}
+                name={name}
+                label={label}
+                invalidDateMessage="Неверная дата"
+                fullWidth
+                variant="inline"
+                value={selectedDate}
+                onChange={setValue}
+                format={'dd.MM.yyyy'}
+                {...rest}
+            />
+            {touched && error && <span className="error_msg">{error}</span>}
+        </>
+    );
+};
+
+export default RenderDateInput;
