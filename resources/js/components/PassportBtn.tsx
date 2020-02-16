de17e91a6f03cc464245de811fa2efa6f976e557
Mi-lex <mishin-lex@mail.ru>
2020-02-16 19:22:03 +0300

    use makestyles instead of css module in passport btn

--- resources/js/components/PassportBtn.tsx
+++ resources/js/components/PassportBtn.tsx
@@ -1,23 +1,58 @@
 import React, { MouseEventHandler } from 'react';
 import icons from '../assets/icons';
 import { IconName, ElementSize } from '../store/types';
-import classes from '../../css/modules/PassportBtn.module.css';
 import { classesExtractor } from '../utils';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) => {
+    const mdSize = 40;
+    const smSize = 24;
+
+    return {
+        btn: {
+            backgroundColor: 'transparent',
+            border: 'none',
+            padding: 0,
+        },
+        md: {
+            width: mdSize,
+            height: mdSize,
+        },
+        sm: {
+            width: smSize,
+            height: smSize,
+        },
+        icon: {
+            maxWidth: '100%',
+            maxHeight: '100%',
+            fill: theme.palette.primary.main,
+            '&:hover': {
+                fill: theme.palette.primary.dark,
+            },
+        },
+    };
+});
 
 type Props = {
+    className?: string;
     iconName: IconName;
     size: ElementSize;
     onClick?: MouseEventHandler;
-    className?: string;
+    style?: object;
 };
 
-const PassportBtn = (props: Props): JSX.Element => {
-    const { className = '', iconName, size = 'md', onClick } = props;
-
-    const btnClassNames = ['btn', `btn--${size}`];
+const PassportBtn: React.FC<Props> = (props: Props): JSX.Element => {
+    const { className = '', iconName, size = 'md', onClick, ...restProps } = props;
+    const classes = useStyles();
+    const btnClassNames = ['btn', size];
 
     return (
-        <button type="button" className={`${classesExtractor(classes, btnClassNames)} ${className}`} onClick={onClick}>
+        <button
+            type="button"
+            className={`${classesExtractor(classes, btnClassNames)} ${className}`}
+            onClick={onClick}
+            {...restProps}
+        >
             <svg className={classes.icon}>
                 <use xlinkHref={icons[iconName]}></use>
             </svg>
