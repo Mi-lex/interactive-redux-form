40228ac52945f5fcebfa74ae952c93ea15783e33
Mi-lex <mishin-lex@mail.ru>
2020-02-19 21:05:17 +0300

    make autosizable textarea component

---
+++ resources/ts/components/MaterialReduxForm/TextareaAutosize.tsx
@@ -0,0 +1,44 @@
+import React, { Ref } from 'react'
+import { WrappedFieldProps } from 'redux-form'
+import TextField, { TextFieldProps } from '@material-ui/core/TextField'
+import TextareaAutosize, { TextareaAutosizeProps } from '@material-ui/core/TextareaAutosize'
+
+type AutoSizeProps = TextareaAutosizeProps & {
+    inputRef: any
+}
+
+const renderTextareaAutosize: React.FC<AutoSizeProps> = props => {
+    const { inputRef, ...custom } = props
+
+    return <TextareaAutosize {...custom} />
+}
+
+type Props = WrappedFieldProps &
+    TextFieldProps & {
+        rowsMin: number | string
+    }
+
+const TextareaAutosizeField: React.FC<Props> = props => {
+    const {
+        label,
+        input,
+        rowsMin,
+        meta: { touched, invalid, error },
+        ...custom
+    } = props
+
+    return (
+        <TextField
+            {...input}
+            InputProps={{
+                inputComponent: renderTextareaAutosize as any,
+                inputProps: { rowsMin },
+            }}
+            label={label}
+            error={touched && invalid}
+            {...custom}
+        />
+    )
+}
+
+export default TextareaAutosizeField
