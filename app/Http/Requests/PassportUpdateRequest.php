322fb138ad89ecc38d4d8a65fe9c07589936d86f
Mi-lex <mishin-lex@mail.ru>
2020-03-06 00:03:45 +0300

    make separate request class for validation

---
+++ app/Http/Requests/PassportUpdateRequest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\PaperJoiner;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class PassportUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $paperJoinerNames = PaperJoiner::NAMES;
+
+        return [
+            'name' => ['string'],
+            'type' => ['string'],
+            'important_info' => ['string'],
+            'completion_date' => ['date_format:d.m.y'],
+            'completion_time' => ['date_format:H:i'],
+            // implementation
+            'is_cut' => ['boolean'],
+            'circulation' => ['string'],
+            "similar_order_id" => ['numeric'],
+            // customer
+            'customer.name' => ['string'],
+            // payment
+            'payment.payed_by_cash' => ['boolean'],
+            'payment.org_type' => ['string', 'exists|payment_org_types,name'],
+            'payment.operation.account_number' => ['numeric', 'required_with:payment.org_type'],
+            'payment.operation.date' => ['numeric', 'required_with:payment.org_type'],
+            // elements
+            'elements' => ['array'],
+            'elements.*.name' => ['string', 'required'],
+            'elements.*.stripes' => ['string', 'required'],
+            'elements.*.material' => ['string', 'required'],
+            'elements.*.print_type' => ['string', 'required', 'exists:print_types,name'],
+            'elements.*.brightness' => ['string', 'required'],
+            'elements.*.color_interpretation' => ['string', 'required'],
+            // paper joiner
+            'paper_joiner' => ['array'],
+            'paper_joiner.name' => ['string', 'required', Rule::in($paperJoinerNames)],
+            /**
+             * if it's paper_clip this line means that type name should exists in papre_clip_types table
+             * if it's glue_bonding it's gonna check glue_bonding_types table etc
+             */
+            'paper_joiner.body.type' => ['string', "exists:" . $this->paper_joiner_name . '_types,name'],
+            'paper_joiner.body' => ['array'],
+            //  * type === 'paper_clip'
+            'paper_joiner.body.auto' => ['boolean'],
+            'paper_joiner.body.manual' => ['boolean'],
+            'paper_joiner.body.width' => ['numeric'],
+            'paper_joiner.body.drift' => ['numeric'],
+            // * type === 'termo'
+            'paper_joiner.body.spine_width' => ['numeric'],
+            'paper_joiner.body.flaps_cover' => ['boolean'],
+            'paper_joiner.body.flush_with_block' => ['boolean'],
+            'paper_joiner.body.braces' => ['boolean'],
+            // * type === 'spring'
+            'paper_joiner.body.color' => ['string'],
+            'paper_joiner.body.position' => ['string'],
+            'paper_joiner.body.cover_block_difference' => ['string'],
+            // * type === 'packet'
+            'paper_joiner.body.grommet_color' => ['string'],
+            'paper_joiner.body.hands_color' => ['string'],
+            // * type === 'glue_bonding'
+            'paper_joiner.body.special' => ['string'],
+        ];
+    }
+}
