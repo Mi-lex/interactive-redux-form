05157fa6d2943abedd31a0d0ee88257f43422fab
Mi-lex <mishin-lex@mail.ru>
2020-03-07 14:32:10 +0300

    update abstract class model to use fill and update method with type names

--- app/AbstractClasses/ModelWithType.php
+++ app/AbstractClasses/ModelWithType.php
@@ -6,21 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 abstract class ModelWithType extends Model
 {
-    private $TypeClassName;
+    private string $TypeClassName;
 
     public function __construct()
     {
         $this->TypeClassName = get_class($this) . 'Type';
     }
 
-    protected function type()
+    public function type()
     {
         return $this->belongsTo($this->TypeClassName);
     }
 
-    public function associateTypeByName($typeName)
+    public function associateTypeByName(string $typeName, ?string $typeTable = null)
     {
-        $typeModel = $this->TypeClassName::whereName($typeName)->first();
-        $this->type()->associate($typeModel);
+        if ($typeTable) {
+            $typeModel = \DB::table($typeTable)->whereName($typeName)->first();
+        } else {
+            $typeModel = $this->TypeClassName::whereName($typeName)->first();
+        }
+
+        return $this->type()->associate($typeModel);
+    }
+
+    // update method uses fill method
+    public function fill(array $attributes)
+    {
+        $typeName = isset($attributes['type']) ? $attributes['type'] : null;
+        unset($attributes['type']);
+
+        parent::fill($attributes);
+
+        if ($typeName) {
+            $this->associateTypeByName($typeName);
+        }
+
+        return $this;
     }
 }
