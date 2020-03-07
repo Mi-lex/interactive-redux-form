4f11a76e1323b69a425726365f13ab13b6106980
Mi-lex <mishin-lex@mail.ru>
2020-03-07 16:16:29 +0300

    use default string length for schemas to fix old sql services error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use App\Models\PaperJoiner;
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        
         $paperJoinersPath = 'App\Models\PaperJoiners';
 
         Relation::morphMap([
