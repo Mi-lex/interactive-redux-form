3ba5e18c970408e4ef35ef6a84bc12aeeac6647f
Mi-lex <mishin-lex@mail.ru>
2020-03-17 23:33:58 +0300

    remove redundent comments

--- resources/ts/utils.ts
+++ resources/ts/utils.ts
@@ -22,36 +22,3 @@ export const getDateString = (date: Date): string => {
 };
 
 export const generateUniqueId = (): string => (Date.now() + Math.random()).toString(36);
-
-/**
- * function for conditionally joining classNames together
- * @param args
- */
-
-// export const classNames = (...args) => {
-//     const classes: Array<number | string> = [];
-
-//     for (let i = 0; i < args.length; i++) {
-//         const arg = args[i];
-//         if (!arg) continue;
-
-//         const argType = typeof arg;
-
-//         if (argType === 'string' || argType === 'number') {
-//             classes.push(arg);
-//         } else if (Array.isArray(arg) && arg.length) {
-//             const inner = classNames(...arg);
-//             if (inner) {
-//                 classes.push(inner);
-//             }
-//         } else if (argType === 'object') {
-//             for (const key in arg) {
-//                 if (Object.prototype.hasOwnProperty.call(arg, key) && arg[key]) {
-//                     classes.push(key);
-//                 }
-//             }
-//         }
-//     }
-
-//     return classes.join(' ');
-// };
