06219ccb271a18849b6d249f0af1dde460b82faf
Mi-lex <mishin-lex@mail.ru>
2020-03-26 14:50:23 +0300

    update cors

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
             ->header('Access-Control-Allow-Headers', '*', 'X-Requested-With', 'Content-Type', 'X-Token-Auth', 'content-type')
             ->header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, OPTIONS');
     }
