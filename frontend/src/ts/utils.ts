44a6a8f946e5cbe709ac0a5778d7a973a3893705
Mi-lex <mishin-lex@mail.ru>
2020-03-26 15:02:48 +0300

    remove unused functions

--- frontend/src/ts/utils.ts
+++ frontend/src/ts/utils.ts
@@ -1,24 +1,15 @@
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+
 /**
- * Function for concatinating several class names from classes obj
+ * Function for concatenating several class names from classes obj
  */
-export const classesExtractor = (classes: any, classNames: string[]): string => {
-    return classNames.map(name => classes[name]).join(' ');
-};
+export const classesExtractor = (
+	classes: any,
+	classNames: string[],
+): string => {
+	return classNames.map((name) => classes[name]).join(' ')
+}
 
-export const getDateString = (date: Date): string => {
-    const day: string = date
-        .getDate()
-        .toString()
-        .padStart(2, '0');
-
-    const month: string = (date.getMonth() + 1).toString().padStart(2, '0');
-
-    const year: string = date
-        .getUTCFullYear()
-        .toString()
-        .slice(0, -2);
-
-    return `${day}.${month}.${year}`;
-};
-
-export const generateUniqueId = (): string => (Date.now() + Math.random()).toString(36);
+export const generateUniqueId = (): string =>
+	(Date.now() + Math.random()).toString(36)
