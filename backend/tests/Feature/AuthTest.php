ccb95443e6c0e72b952f768bcf0749a4477f02d7
Mi-lex <mishin-lex@mail.ru>
2020-03-27 18:38:24 +0300

    remove unused  classes

--- backend/tests/Feature/AuthTest.php
+++ backend/tests/Feature/AuthTest.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class AuthTest extends TestCase
