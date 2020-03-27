341062768a06bc979134824fced40afa74030c93
Mi-lex <mishin-lex@mail.ru>
2020-03-27 12:15:11 +0300

    add registration route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,6 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/register', 'Auth\RegisterController@register');
+
 Route::group([
     'prefix' => 'passport',
 ], function () {
