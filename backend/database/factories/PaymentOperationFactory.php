1a09311f17968992d3dce12b68e3d6363f3b1800
Mi-lex <mishin-lex@mail.ru>
2020-04-01 13:15:59 +0300

    test refresh and expire token requests, fix previous ones

--- backend/database/factories/PaymentOperationFactory.php
+++ backend/database/factories/PaymentOperationFactory.php
@@ -2,16 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use Carbon\Carbon;
-use App\Models\PaymentOperation;
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use App\Models\PaymentOperation;
 
 $factory->define(PaymentOperation::class, function (Faker $faker) {
     return [
         "account_number" => $faker->randomDigit,
         "date" => $faker->date,
         "org_type" => function () use ($faker) {
-            return \App\Models\PaymentOrgType::create(["alias" => $faker->word, "name" => $faker->word])->alias;
+            return \App\Models\PaymentOrgType::create(["alias" => Str::random(5), "name" => $faker->word])->alias;
         },
         "payment_id" => function () {
             return factory('App\Models\Payment')->create(['payed_by_cash' => false])->id;
--- backend/tests/Feature/AuthTest.php
+++ backend/tests/Feature/AuthTest.php
@@ -6,12 +6,15 @@ use Tests\TestCase;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
+use App\Traits\InteractsWithAuthOverrides;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use JWTAuth;
 
 class AuthTest extends TestCase
 {
-    use DatabaseMigrations, WithFaker;
+    use DatabaseMigrations, WithFaker, InteractsWithAuthOverrides;
 
     protected function setUp(): void
     {
@@ -26,11 +29,16 @@ class AuthTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $formCredentials = factory(User::class)->raw(['password' => Str::random(10)]);
+        $password = Str::random(10);
+
+        $formCredentials = factory(User::class)->raw();
+        $formCredentials['password'] = $password;
+        $formCredentials['password_confirmation'] = $password;
 
         $this->post('/api/auth/register', $formCredentials)->assertStatus(201);
 
         unset($formCredentials['password']);
+        unset($formCredentials['password_confirmation']);
         unset($formCredentials['remember_token']);
 
         $this->assertDatabaseHas('users', $formCredentials);
@@ -63,18 +71,41 @@ class AuthTest extends TestCase
             'password' => $password
         ];
 
-        $this->post('/api/auth/login', $userCredentials)->assertOk()->assertJsonStructure([
-            'access_token',
-            'token_type',
-            'expires_in'
-        ]);
+        $response = $this->post('/api/auth/login', $userCredentials)->assertOk();
+        $this->assertNotEmpty($response->headers->get('Authorization'));
     }
 
     /** @test */
-    public function an_authorized_user_cannot_access_protected_route()
+    public function an_unauthorized_user_cannot_access_protected_route()
     {
         $protected = 'api/orders';
 
         $this->get($protected, ['accept' => 'application/json'])->assertStatus(401);
+    }
+
+    /** @test */
+    public function an_authorized_user_can_refresh_expired_token()
+    {
+        $this->actingAs(factory('App\Models\User')->create());
+
+        Carbon::setTestNow(Carbon::now()->addMinutes(auth()->factory()->getTTL()));
+
+        $response = $this->post('api/auth/refresh')->assertOk();
+        $this->assertNotEmpty($response->headers->get('Authorization'));
+    }
+
+    /** @test */
+    public function an_authorized_user_cannot_refresh_expired_token_past_refresh_time()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs(factory('App\Models\User')->create());
+
+        // there is no getRefreshTTL method, so I just piked the value from config/jwt.php
+        $configRefreshTimeTTL = 20160; // [minutes] = two weeks
+        Carbon::setTestNow(Carbon::now()->addMinutes($configRefreshTimeTTL));
+
+        $response = $this->post('api/auth/refresh')->assertStatus(401);
+        $this->assertNull($response->headers->get('Authorization'));
     }
 }
