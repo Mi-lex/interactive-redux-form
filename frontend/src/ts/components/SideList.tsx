0ca811598966de7634231d670eac061f5c80dd65
Mi-lex <mishin-lex@mail.ru>
2020-04-03 17:29:54 +0300

    update nav list

--- frontend/src/ts/components/SideList.tsx
+++ frontend/src/ts/components/SideList.tsx
@@ -1,66 +1,66 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Divider from '@material-ui/core/Divider'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import { makeStyles } from '@material-ui/core/styles'
 import DescriptionIcon from '@material-ui/icons/Description'
-import MailIcon from '@material-ui/icons/Mail'
-import InboxIcon from '@material-ui/icons/MoveToInbox'
-import TocIcon from '@material-ui/icons/Toc'
-
-const useStyles = makeStyles({
-	list: {
-		width: 250,
-	},
-	fullList: {
-		width: 'auto',
-	},
-})
+import Box from '@material-ui/core/Box'
+import OrderIcon from '@material-ui/icons/ShoppingCart'
+import CustomerIcon from '@material-ui/icons/People'
+import BarChartIcon from '@material-ui/icons/BarChart'
+import AssignmentIcon from '@material-ui/icons/Assignment'
 
 type Props = {
 	closeDrawer: () => void
 }
 
-const SideList = (props: Props): JSX.Element => {
-	const classes = useStyles()
-	const { closeDrawer } = props
+const SideList: React.FC<Props> = ({ closeDrawer }) => {
+	const menuLinkItems = [
+		{
+			path: '/passport',
+			name: 'Пасспорт',
+			IconComponent: <DescriptionIcon />,
+		},
+		{
+			path: '/',
+			name: 'Заказы',
+			IconComponent: <OrderIcon />,
+		},
+		{
+			path: '#',
+			name: 'Клиенты',
+			IconComponent: <CustomerIcon />,
+		},
+		{
+			path: '#',
+			name: 'Статистика',
+			IconComponent: <BarChartIcon />,
+		},
+		{
+			path: '#',
+			name: 'Канбан доска',
+			IconComponent: <AssignmentIcon />,
+		},
+	]
 
 	return (
-		<div
-			className={classes.list}
+		<Box
+			component={'div'}
+			width={250}
 			role="presentation"
 			onClick={closeDrawer}
 			onKeyDown={closeDrawer}
 		>
 			<List>
-				<ListItem button component={Link} to="/passport">
-					<ListItemIcon>
-						<DescriptionIcon />
-					</ListItemIcon>
-					<ListItemText primary="Паспорт" />
-				</ListItem>
-				<ListItem button component={Link} to="/">
-					<ListItemIcon>
-						<TocIcon />
-					</ListItemIcon>
-					<ListItemText primary="Заказы" />
-				</ListItem>
-				<Divider />
-				{['Статистика', 'Канбан доска', 'Активность', 'Последняя ссылка'].map(
-					(text, index) => (
-						<ListItem button key={text}>
-							<ListItemIcon>
-								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-							</ListItemIcon>
-							<ListItemText primary={text} />
-						</ListItem>
-					),
-				)}
+				{menuLinkItems.map(({ path, name, IconComponent }) => (
+					<ListItem button component={Link} to={path} key={name}>
+						<ListItemIcon>{IconComponent}</ListItemIcon>
+						<ListItemText primary={name} />
+					</ListItem>
+				))}
 			</List>
-		</div>
+		</Box>
 	)
 }
 
